dcf4510a319d2bf8f0067ec9db3636fd76815e94
LuisDavidPortillaBarco <luisportilla009@gmail.com>
2019-10-19 07:41:32 -0500

    drwer

--- app/src/main/java/com/example/calc/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/calc/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,15 +15,22 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.calc.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
-    private HomeViewModel homeViewModel;
+    private Button b0,b1,b2,b3,b4,b5,b6,b7,b8,b9,bpl,bmi,bmu,bdi,bpo,beq,bc,bce;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        return root;
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        b0 = view.findViewById(R.id.button_0);
+        b1 = 
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
